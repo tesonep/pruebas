fb7980770c55c64add3f325afbb6e0c7250bf9f6
Pablo Tesone <tesonep@gmail.com>
2012-03-07 15:59:56 -0300

    Euler

--- euler/euler31.hs
+++ euler/euler31.hs
@@ -1,4 +1,27 @@
-p31_coins = [1,2,5,10,20,50,100,200]
-p31_posibles 0 = []
-p31_posibles n = filter (<=n) p31_coins
+import List
 
+expand xs = sort $ expand' xs xs
+expand' xs acc = if nl == [] then acc else (expand' nl (acc ++ nl))
+	    	where nl = compress $ nextLevel xs
+
+nextLevel :: [[Integer]] -> [[Integer]]
+nextLevel [] = []
+nextLevel (x:xs) = nextLevel' [] x ++ nextLevel xs
+
+reemplazos = [
+		(2, [1,1]),
+		(5, [2,2,1]),
+		(10, [5,5]),
+		(20, [10,10]),
+		(50, [20,20,10]),
+		(100, [50,50]),
+		(200, [100,100])
+	     ]
+
+
+nextLevel' :: [Integer] -> [Integer] -> [[Integer]]
+nextLevel' _ []  = []
+nextLevel' acc (1:xs) = nextLevel' (acc ++ [1]) xs
+nextLevel' acc (x:xs) = compress $ sort (acc ++ reemplazo ++ xs) : nextLevel' (acc ++ [x]) xs 
+				where reemplazo = maybe [] id (lookup x reemplazos)
+compress = map head . group
